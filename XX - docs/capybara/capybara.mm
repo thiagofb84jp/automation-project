94a8250fcacd93af469db77c60a3da87bc9f73fd
Thiago Ferreira Barbosa <thiago.barbosa@controlconstrucoes.com.br>
2016-10-07 11:48:46 -0300

    Criação dos Arquivos de Configuração para o projeto Capybara

---
+++ XX - docs/capybara/capybara.mm
@@ -0,0 +1,30 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1475842054285" ID="ID_1994376288" MODIFIED="1475851487442" TEXT="CAPYBARA">
+<font NAME="Impact" SIZE="28"/>
+<node CREATED="1475847198939" ID="ID_266099324" MODIFIED="1475847852869" POSITION="right" TEXT="o que &#xe9;">
+<icon BUILTIN="help"/>
+<node CREATED="1475847854083" ID="ID_124571399" MODIFIED="1475848440402" TEXT="Ferramenta de automa&#xe7;&#xe3;o para testes funcionais, &#xe9; uma biblioteca escrita em Ruby para simular as a&#xe7;&#xf5;es do usu&#xe1;rio sobre o sistema testado">
+<icon BUILTIN="idea"/>
+</node>
+<node CREATED="1475850352056" ID="ID_540716591" MODIFIED="1475850397174" TEXT="Fornece uma camada de abstra&#xe7;&#xe3;o para a biblioteca de automa&#xe7;&#xe3;o. Funciona como um tradutor, onde realiza a comunica&#xe7;&#xe3;o entre a API e o driver de automa&#xe7;&#xe3;o.">
+<icon BUILTIN="info"/>
+</node>
+</node>
+<node CREATED="1475847899059" ID="ID_1286127456" MODIFIED="1475847904445" POSITION="left" TEXT="quais caracter&#xed;sticas">
+<icon BUILTIN="help"/>
+<node CREATED="1475847980199" ID="ID_103269339" MODIFIED="1475848428731" TEXT="Pode ser executada em qualquer navegador">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1475848059331" ID="ID_537175946" MODIFIED="1475849043126" TEXT="Utiliza os princ&#xed;pios do BDD juntamente com algum framework de automa&#xe7;&#xe3;o (Selenium Webdriver, Sinatra, etc.)">
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1475848161420" ID="ID_1713714441" MODIFIED="1475848433139" TEXT="N&#xe3;o &#xe9; preciso inserir tempos/intervalos de espera para os componentes da p&#xe1;gina aparece (Javascript, Action Script, entre outros)">
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1475848644716" ID="ID_275793562" MODIFIED="1475848680491" TEXT="Possui f&#xe1;cil escrita e manuten&#xe7;&#xe3;o">
+<icon BUILTIN="full-4"/>
+</node>
+</node>
+</node>
+</map>
