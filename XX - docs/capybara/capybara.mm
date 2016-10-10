ef559a62e8fcf95c35a55521bcc8d819f4df1503
Thiago Ferreira Barbosa <thiago.barbosa@controlconstrucoes.com.br>
2016-10-10 10:15:57 -0300

    Atualização da documentação do projeto Cucumber

--- XX - docs/capybara/capybara.mm
+++ XX - docs/capybara/capybara.mm
@@ -1,17 +1,20 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1475842054285" ID="ID_1994376288" MODIFIED="1475851487442" TEXT="CAPYBARA">
+<node CREATED="1475842054285" ID="ID_1994376288" MODIFIED="1476101422510" TEXT="CAPYBARA">
 <font NAME="Impact" SIZE="28"/>
-<node CREATED="1475847198939" ID="ID_266099324" MODIFIED="1475847852869" POSITION="right" TEXT="o que &#xe9;">
+<node CREATED="1475847198939" ID="ID_266099324" MODIFIED="1476105038673" POSITION="right" TEXT="o que &#xe9;">
+<font NAME="SansSerif" SIZE="14"/>
 <icon BUILTIN="help"/>
-<node CREATED="1475847854083" ID="ID_124571399" MODIFIED="1475848440402" TEXT="Ferramenta de automa&#xe7;&#xe3;o para testes funcionais, &#xe9; uma biblioteca escrita em Ruby para simular as a&#xe7;&#xf5;es do usu&#xe1;rio sobre o sistema testado">
+<node CREATED="1475847854083" ID="ID_124571399" MODIFIED="1476105067457" TEXT="Ferramenta de automa&#xe7;&#xe3;o para testes funcionais, &#xe9; uma biblioteca escrita em Ruby para simular as a&#xe7;&#xf5;es do usu&#xe1;rio sobre o sistema testado">
+<font NAME="SansSerif" SIZE="12"/>
 <icon BUILTIN="idea"/>
 </node>
 <node CREATED="1475850352056" ID="ID_540716591" MODIFIED="1475850397174" TEXT="Fornece uma camada de abstra&#xe7;&#xe3;o para a biblioteca de automa&#xe7;&#xe3;o. Funciona como um tradutor, onde realiza a comunica&#xe7;&#xe3;o entre a API e o driver de automa&#xe7;&#xe3;o.">
 <icon BUILTIN="info"/>
 </node>
 </node>
-<node CREATED="1475847899059" ID="ID_1286127456" MODIFIED="1475847904445" POSITION="left" TEXT="quais caracter&#xed;sticas">
+<node CREATED="1475847899059" ID="ID_1286127456" MODIFIED="1476105035537" POSITION="left" TEXT="quais caracter&#xed;sticas">
+<font NAME="SansSerif" SIZE="14"/>
 <icon BUILTIN="help"/>
 <node CREATED="1475847980199" ID="ID_103269339" MODIFIED="1475848428731" TEXT="Pode ser executada em qualquer navegador">
 <icon BUILTIN="full-1"/>
