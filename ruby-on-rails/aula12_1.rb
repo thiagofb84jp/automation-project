9ab9dc28e99ea57d6bd2800eb4e3d68b0422cbc0
Thiago Barbosa <thiago.barbosa@control.eng.br>
2016-12-07 17:58:34 -0300

    Atualização dos projetos de Ruby on Rails e documentação do Ruby

---
+++ ruby-on-rails/aula12_1.rb
@@ -0,0 +1,7 @@
+puts "Digite seu nome..."
+nome = gets.chomp
+puts nome
+
+puts "Digite sua idade..."
+idade = gets.to_i
+puts idade + 3
---
+++ ruby-on-rails/aula12_2.rb
@@ -0,0 +1,23 @@
+#Condicionais
+
+a = 3
+b = 5
+
+if a < b
+	puts "a é menor"
+else
+	puts "a é maior"
+end
+
+unless a < b
+	puts "b é menor"
+else
+	puts "a é menor"
+end
+
+case a
+when 3
+	puts "É três"
+when 4
+	puts "É quatro"
+end	
