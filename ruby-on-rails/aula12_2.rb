a7c5fb006998e803f0478e64db48ff7eb0725041
thiagofb84jp <thiagofb84jp@gmail.com>
2016-12-08 09:29:13 -0300

    Atualização projeto Ruby on Rails

--- ruby-on-rails/aula12_2.rb
+++ ruby-on-rails/aula12_2.rb
@@ -15,9 +15,43 @@ else
 	puts "a é menor"
 end
 
+#case condicional
 case a
 when 3
-	puts "É três"
+	puts "é três"
 when 4
-	puts "É quatro"
+	puts "é quatro"
 end	
+
+#condicional ternário
+(a == 3) ? (puts "é igual a 3") : (puts "é diferente!")
+
+#operadores aritméticos
+puts 1 + 2
+puts 2 * 2
+puts 5 - 3
+puts 4 / 4
+puts 2**3
+puts 5%1
+
+#Enquanto que
+while a < 10
+	a += 1
+	puts a
+end
+
+#Até que
+until a == 0
+	a-=1
+	puts a
+end
+
+#For
+for i in 0..5
+	puts i
+end
+
+#Pegue cada elemento da lista e imprima
+[1,2,3,4].each do |j|
+	puts j
+end
