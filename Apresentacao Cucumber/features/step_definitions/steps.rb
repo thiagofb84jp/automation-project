854e9a9a8961bacc11e712bdb3b1bd3acb2afc1f
thiagofb84jp <thiagofb84jp@gmail.com>
2018-04-04 21:11:59 -0300

    Atualização da apresentação Cucumber

--- Apresentacao Cucumber/features/step_definitions/steps.rb
+++ Apresentacao Cucumber/features/step_definitions/steps.rb
@@ -7,8 +7,8 @@ Quando(/^cadastrar um novo usuário$/) do
 end
 
 Quando(/^informar todos os campos obrigatórios e confirmar os dados$/) do
-  fill_in 'nome', :with => 'Joseana Lima Dias Duarte Paiva'
-  fill_in 'email', :with => 'joseana_paiva@gmail.com'
+  fill_in 'nome', :with => 'João Lima'
+  fill_in 'email', :with => 'joao_lima@gmail.com'
   fill_in 'senha', :with => 'abcd_123'
   click_on 'Cadastrar'
 end
@@ -17,12 +17,33 @@ Então(/^vejo uma mensagem informando que o usuário foi cadastrado com sucesso$
   expect(page).to have_content 'Usuário inserido com sucesso'
 end
 
+Quando(/^cadastrar um novo usuário sem informar os campos obrigatórios e confirmar os dados$/) do
+  click_link_or_button 'Novo usuário?'
+  click_on 'Cadastrar'
+end
+
+Então(/^vejo uma mensagem destacando todos os campos obrigatórios para preenchimento$/) do
+  expect(page).to have_content 'Nome é um campo obrigatório'
+  expect(page).to have_content 'Email é um campo obrigatório'
+  expect(page).to have_content 'Senha é um campo obrigatório'
+end
+
 Quando(/^informar um usuário e senha válidos$/) do
-  fill_in 'email', :with => 'joseana_paiva@gmail.com'
+  fill_in 'email', :with => 'joao_lima@gmail.com'
   fill_in 'senha', :with => 'abcd_123'
   click_on 'Entrar'
 end
 
 Então(/^o sistema deve permitir a autenticação do usuário$/) do
-  expect(page).to have_content 'Bem vindo, Joseana Lima Dias Duarte Paiva!'
+  expect(page).to have_content 'Bem vindo, João Lima!'
+end
+
+Quando(/^informar um usuário e senha inválidos$/) do
+  fill_in 'email', :with => 'carla_lima@gmail.com'
+  fill_in 'senha', :with => 'abcd_888'
+  click_on 'Entrar'
+end
+
+Então(/^o sistema não deve permitir a autenticação exibindo os campos obrigatórios para preenchimento$/) do
+  expect(page).to have_content 'Problemas com o login do usuário'
 end
