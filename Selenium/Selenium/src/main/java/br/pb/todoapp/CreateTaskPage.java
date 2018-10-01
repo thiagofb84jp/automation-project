f882accde9b6cf32f62c8536560b2da7668d82d2
thiago.barbosa@shift.com.br <Filosofia_123>
2018-10-01 05:53:40 -0300

    Criação de novos métodos para a classe CreateTaskPage.java

--- Selenium/Selenium/src/main/java/br/pb/todoapp/CreateTaskPage.java
+++ Selenium/Selenium/src/main/java/br/pb/todoapp/CreateTaskPage.java
@@ -1,5 +1,6 @@
 package br.pb.todoapp;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
@@ -15,7 +16,26 @@ public class CreateTaskPage extends PageObject{
 		driver.findElement(By.id("new_task")).sendKeys(descricao);
 	}
 	
-	public void salvarRegistro() {
+	public void adicionarTarefa() {
 		driver.findElement(By.xpath("//span[@class='input-group-addon glyphicon glyphicon-plus']")).click();
 	}
+
+	public void obterLink(String link) {
+		driver.findElement(By.linkText(link)).click();	
+	}
+	
+	public void gerenciarSubtarefas() {
+		driver.findElement(By.xpath("//table/tbody/tr[1]/td[4]/button")).click();
+	}
+	
+	public void validarDescricaoTarefa(String valorResultado) {
+		String getResultado = driver.findElement(By.xpath("//div[@class='alert alert-info']")).getText();
+		Assert.assertEquals(valorResultado, getResultado);
+	}
+	
+	/*
+	 * Semana que vem (08/10):
+	 * - Executar o primeiro caso de teste;
+	 * - Procurar um método genérico para clicar nas subtarefas;
+	 */
 }
--- Selenium/Selenium/src/main/java/br/pb/todoapp/CreateTaskTeste.java
+++ Selenium/Selenium/src/main/java/br/pb/todoapp/CreateTaskTeste.java
@@ -1,7 +1,44 @@
 package br.pb.todoapp;
 
+import org.junit.Test;
+
 import br.pb.core.FuncionalTest;
 
 public class CreateTaskTeste extends FuncionalTest{
 	
+	private static final String URL_TODO_APP = "https://qa-test.avenuecode.com/";
+
+	SignInPage signInPage = new SignInPage(driver);
+	CreateTaskPage createTaskPage = new CreateTaskPage(driver);
+
+	public CreateTaskTeste() {
+		driver.get(URL_TODO_APP);
+		signInPage.obterLink("Sign In");
+		signInPage.obterEmail("hhenriquejorgetomasdamata@netpont.com.br");
+		signInPage.obterSenha("abcd_123");
+		signInPage.logar();
+		
+		createTaskPage.obterLink("My Tasks");
+	}
+	
+	@Test
+	public void adicionarTarefa() {
+		createTaskPage.informarDescricaoTarefa("Fazer Compras no Supermercados");
+		createTaskPage.adicionarTarefa();
+	}
+	
+	@Test
+	public void adicionarSubtarefas() {
+		
+	}
+	
+	@Test
+	public void removerTarefas() {
+		
+	}
+	
+	@Test
+	public void removerSubtarefas() {
+		
+	}	
 }
