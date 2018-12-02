e4f554ea9c9e0effdc6d0f463705cad273dd25ac
thiagofb84jp <thiagofb84jp@gmail.com>
2018-12-02 21:42:20 -0200

    Atualização dos projetos de automação

--- Manter Funcionarios Projeto/manter-funcionarios/src/main/java/br/com/desafiofuncionario/enums/Ocupacao.java
+++
@@ -1,8 +0,0 @@
-package br.com.desafiofuncionario.enums;
-
-public enum Ocupacao {
-
-	ATENDENTE,
-	COORDENADOR,
-	GERENTE
-}
--- Manter Funcionarios Projeto/manter-funcionarios/src/main/java/br/com/desafiofuncionario/enums/Setor.java
+++
@@ -1,8 +0,0 @@
-package br.com.desafiofuncionario.enums;
-
-public enum Setor {
-
-	RH,
-	ATENDIMENTO,
-	FINANCEIRO
-}
