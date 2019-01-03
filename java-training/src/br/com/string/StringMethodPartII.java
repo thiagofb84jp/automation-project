2d9ec22c28a8499e3d0ed5d6ec951c47fcbd5d04
thiagofb84jp <thiagofb84jp@gmail.com>
2019-01-03 12:59:35 -0300

    Atualização do projeto de testes automatizados

---
+++ java-training/src/br/com/string/StringMethodPartII.java
@@ -0,0 +1,33 @@
+package br.com.string;
+
+public class StringMethodPartII {
+	
+	public static void main(String[] args) {
+		String text1 = "TechTraining is good";
+		
+		/*
+		 * Learning about the method "String equals()"
+		String text2 = "TechTraining";
+		String text3 = "techtraining";
+		
+		System.out.println(text1.equals(text2));
+		System.out.println(text1.equals(text3));
+		
+		System.out.println(text1.equalsIgnoreCase(text3));*/
+		
+		/*
+		 * Learning about the method "String indexOf()" 
+		System.out.println("Index of value = " + text1.indexOf("Training"));
+		System.out.println("Index of value = " + text1.indexOf("java"));
+		*/
+		
+		/*
+		 * Learning about the method "String lastIndexOf()" 
+		System.out.println(text1.lastIndexOf("T"));
+		*/
+		
+		/*
+		 * Learning about the method "String length()*/
+		System.out.println(text1.length());
+	}
+}
---
+++ java-training/src/br/com/string/StringMethodPartIII.java
@@ -0,0 +1,34 @@
+package br.com.string;
+
+public class StringMethodPartIII {
+
+	public static void main(String[] args) {
+		//String text = "Hello Team!";
+		String sample = "His name is John and he is a man";
+		
+		/*
+		 * Learning about the method "String replace()"
+		String text1 = text.replace("e", "a");
+		String sample2 = sample.replace("is", "was");
+		
+		System.out.println(text1);
+		System.out.println(sample2);
+		*/
+		
+		/*
+		 * Learning about the method "String split()" 
+		String[] sampleTest = sample.split(" ");
+		
+		System.out.println(sample);
+		
+		for (int i = 0; i < sampleTest.length; i++) {
+			System.out.println(sampleTest[i]);
+		}
+		*/
+		
+		/*
+		 * Learning about the method "String starsWith()" 
+		System.out.println(sample.startsWith("H"));
+		*/
+	}
+}
