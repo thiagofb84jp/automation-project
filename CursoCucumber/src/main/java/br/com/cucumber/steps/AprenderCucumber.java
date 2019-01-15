3c69c08faad7501b0e5721f1d18754f3792e6eb7
thiagofb84jp <thiagofb84jp@gmail.com>
2019-01-15 06:20:45 -0200

    Atualização dos projetos de automação da parte do curso Udemy sobre Cucumber

--- CursoCucumber/src/main/java/br/com/cucumber/steps/AprenderCucumber.java
+++ CursoCucumber/src/main/java/br/com/cucumber/steps/AprenderCucumber.java
@@ -1,4 +1,10 @@
 package br.com.cucumber.steps;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import org.junit.Assert;
 
 import cucumber.api.java.pt.Dado;
@@ -17,14 +23,13 @@ public class AprenderCucumber {
 
 	}
 
-	@Então
-	("^a especificação deve finalizar com sucesso$")
+	@Então("^a especificação deve finalizar com sucesso$")
 	public void a_especificação_deve_finalizar_com_sucesso() throws Throwable {
 
 	}
-	
+
 	private int contador = 0;
-	
+
 	@Dado("^que o valor do contador é (\\d+)$")
 	public void queOValorDoContadorÉ(int arg1) throws Throwable {
 		contador = arg1;
@@ -38,5 +43,37 @@ public class AprenderCucumber {
 	@Então("^o valor do contador será (\\d+)$")
 	public void oValorDoContadorSerá(int arg1) throws Throwable {
 		Assert.assertEquals(arg1, contador);
+	}
+
+	Date entrega = new Date();
+
+	@Dado("^que a entrega é dia (\\d+)/(\\d+)/(\\d+)$")
+	public void queAEntregaÉDia(int dia, int mes, int ano) throws Throwable {
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.DAY_OF_MONTH, dia);
+		cal.set(Calendar.MONTH, mes - 1);
+		cal.set(Calendar.YEAR, ano);
+		entrega = cal.getTime();
+	}
+
+	@Quando("^a entrega atrasar em (\\d+) (dia|dias|mes|meses)$")
+	public void aEntregaAtrasarEmDias(int qtdDiasAtraso, String tempo) throws Throwable {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(entrega);
+		
+		if (tempo.equals("dias")) {
+			cal.add(Calendar.DAY_OF_MONTH, qtdDiasAtraso);
+		}
+		if (tempo.equals("meses")) {
+			cal.add(Calendar.MONTH, qtdDiasAtraso);
+		}
+		entrega = cal.getTime();
+	}
+
+	@Então("^a entrega será efetuada em (\\d{2}\\/\\d{2}\\/\\d{4})$")
+	public void aEntregaSeráEfetuadaEm(String data) throws Throwable {
+		DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+		String dataFormatada = format.format(entrega);
+		Assert.assertEquals(data, dataFormatada);
 	}
 }
--- CursoCucumber/src/test/java/aprender_cucumber.feature
+++ CursoCucumber/src/test/java/aprender_cucumber.feature
@@ -10,11 +10,21 @@ Funcionalidade: Aprender Cucumber
     Então a especificação deve finalizar com sucesso
 
   Cenário: Deve incrementar contador
-  	Dado que o valor do contador é 15
-  	Quando eu incrementar em 3
-  	Então o valor do contador será 18
-  	
+    Dado que o valor do contador é 15
+    Quando eu incrementar em 3
+    Então o valor do contador será 18
+
   Cenário: Deve incrementar contador
-  	Dado que o valor do contador é 123
-  	Quando eu incrementar em 35
-  	Então o valor do contador será 158  	
+    Dado que o valor do contador é 123
+    Quando eu incrementar em 35
+    Então o valor do contador será 158
+
+  Cenário: Deve calcular atraso na entrega
+    Dado que a entrega é dia 05/04/2018
+    Quando a entrega atrasar em 2 dias
+    Então a entrega será efetuada em 07/04/2018
+
+  Cenário: Deve calcular atraso na entrega da China
+    Dado que a entrega é dia 05/04/2018
+    Quando a entrega atrasar em 2 meses
+    Então a entrega será efetuada em 05/06/2018
